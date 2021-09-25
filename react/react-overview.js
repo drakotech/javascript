b9b17d54f5af8f60e0384c27a691cf65fc76b19c
drakotech <george.flefel@gmail.com>
2021-09-25 15:11:49 -0700

    React, a powerful Javascript Library

---
+++ react/react-overview.js
@@ -0,0 +1,24 @@
+/* React Architecture Overview
+
+Reactjs.org
+
+React is a lightweight framework for Javascript. So light-
+weight infact its actually a Javascript library. A library
+so powerful that people actually use it as a framework.
+
+React is component based. That means that react organizes 
+code bases based on components.
+
+Built and developed by Facebook. Every element on a react
+website is called a component, is reusable, can manage
+their own state and be composed into complex UIs.
+
+Instead of building traditional web pages with a series of
+html files, react applications use a single html file. The
+data and all of those components simply slide in and they
+are used and rendered on the screen whenever they are 
+called by the application. When they are not needed they
+simply get knocked off the stack and whatever the user
+wants to see gets popped up.
+
+*/
